f508021a0050a6347deffd060c29979532cd13fe
viziodev <viziodev03@gmail.com>
2024-05-11 17:43:29 +0000

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo/>
+            <Navigation/>
+            <h1>A propos</h1>
+            <br />
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Harum magni totam animi aliquid accusamus labore maxime expedita aliquam quo perferendis beatae ex, nobis asperiores perspiciatis modi porro quos ea officia deserunt atque possimus alias enim quae nesciunt!
+                A consequuntur veniam fuga nulla, similique nihil ipsa velit impedit aperiam minus dolorum, aliquam necessitatibus atque.
+                Non culpa numquam similique est suscipit voluptatum explicabo officiis esse molestias totam tempore vitae quo, perspiciatis harum, aliquid itaque fuga tempora obcaecati laboriosam.
+                Blanditiis distinctio, ea eius neque, ipsum deleniti accusantium nostrum voluptates quae eaque quos, corporis at excepturi est similique doloremque eveniet impedit cum veniam ratione.
+            </p>
+            <br />
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Facere magnam amet voluptates quibusdam ducimus delectus reprehenderit voluptatum totam, non ipsam voluptas alias architecto!
+                Corporis rem sit possimus impedit id aut dicta labore in deleniti iste repudiandae at, molestiae eveniet amet architecto cum dolore, dolorem aliquid deserunt quaerat ab maiores omnis veniam?
+                Voluptate sint earum eum nostrum doloremque quaerat exercitationem in fuga ullam eos velit ipsa, accusantium veritatis quidem libero aliquid laborum reprehenderit asperiores eligendi sequi fugiat saepe adipisci omnis.
+                Cum obcaecati suscipit doloribus ullam! Exercitationem aut sapiente ducimus sit asperiores minus, dolorum maxime earum illo fuga nisi suscipit, quae nesciunt.
+            </p>
+        </div>
+    );
+};
+
+export default About;
